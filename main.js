f219cd8e3427c6d936fafc7de6c66583a8c5b5ad
Bryan Elliott <fordiman@gmail.com>
2014-01-30 17:18:42 -0500

    Finished building parser

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*jslint devel:true */
 var define, brackets, CodeMirror;
 define(function (require, exports, module) {
 	'use strict';
@@ -49,9 +50,8 @@ define(function (require, exports, module) {
 					}
 					if (state.baseCur !== null && combine) {
 						return state.baseCur + " " + state.overlayCur;
-					} else {
-						return state.overlayCur;
 					}
+					return state.overlayCur;
 				},
 
 				indent: base.indent && function (state, textAfter) {
@@ -80,139 +80,149 @@ define(function (require, exports, module) {
 
 	CodeMirror.defineMode("handlebars", function (config, parserConfig) {
 		var mustacheOverlay = {
+			startState: function () {
+				return {
+					moustacheStack: [],
+					hasError: false,
+					errorTerminatesOn: null,
+					opening: false,
+					closing: false,
+					helperName: false
+				};
+			},
 			token: function (stream, state) {
 				/*jslint regexp:true */
-				var ch;
-				if (state.hasError) {
+				stream.eatSpace();
+				if (!state.inHandlebar) {
+					if (stream.eat('{')) {
+						if (stream.eat('{')) {
+							stream.eatSpace();
+							state.inHandlebar = true;
+							state.helperName = true;
+							state.closing = false;
+							state.opening = false;
+							state.doNotEscape = false;
+							if (stream.eat('{')) {
+								//output HTML
+								state.doNotEscape = true;
+								stream.eatSpace();
+								return "operator";
+							}
+							if (stream.eat('#')) {
+								//tag start
+								state.opening = true;
+							} else if (stream.eat("/")) {
+								//tag end
+								state.closing = true;
+							}
+							stream.eatSpace();
+							return "bracket";
+						}
+					}
+					if (stream.next() === null && state.moustacheStack.length > 0) {
+						console.log("Unclosed tags: ", moustacheStack);
+						return "invalidchar";
+					}
 					return null;
 				}
-				if (!state.moustacheStack) {
-					state.moustacheStack = [];
+				if (state.helperName) {
+					state.helperName = false;
+					if (!state.opening && !state.closing && stream.match(/^[\w\d\-\_\$]+\s*\}\}/, false)) {
+						stream.match(/^[\w\d\-\_\$\.]+/, true);
+						stream.eatSpace();
+						return "variable";
+					}
+					if (stream.match(/^[\w\d\-\_\$]+/, false)) {
+						stream.match(/^[\w\d\-\_\$]+/, true);
+						state.helperName = false;
+						if (state.closing) {
+							state.closing = false;
+							state.endOnly = true;
+							if (state.moustacheStack.pop() !== stream.current()) {
+								console.log('Mismatched tags');
+								return "invalidchar";
+							}
+							return "tag";
+						}
+						if (state.opening) {
+							stream.opening = false;
+							state.moustacheStack.push(stream.current());
+						}
+						state.argumentList = true;
+						return "tag";
+					}
+					stream.next();
+					return "invalidchar";
 				}
-				if (!state.inMoustache) {
-					if (stream.match(/\{\{/, true)) {
-						state.inMoustache = true;
-						state.helperName = true;
-						state.checkForTag = true;
+				if (state.endOnly) {
+					state.endOnly = false;
+					if (stream.match('}}', true)) {
+						stream.eatSpace();
+						state.inHandlebar = false;
 						return "bracket";
 					}
-				} else {
-					if (state.checkForTag) {
-						state.checkForTag = false;
-						if (stream.match(/#/, true)) {
-							stream.next();
-							state.opening = true;
-							return "keyword";
-						} else if (stream.match(/\//, true)) {
-							stream.next();
-							state.closing = true;
-							return "keyword";
-						}
-					} else if (state.helperName) {
-						state.helperName = false;
-						ch = (stream.match(/[a-zA-Z0-9\-\_\$]+/, true) || []).join('');
-						if (ch) {
-							if (state.opening) {
-								state.moustacheStack.push(ch);
-								return "tag";
-							} else if (state.closing) {
-								if (state.moustacheStack[state.moustacheStack.length - 1] !== ch) {
-									state.hasError = true;
-									return "invalidchar";
-								}
-								state.moustacheStack.pop();
-								return "tag";
-							}
-						}
-					} else if (state.attributeNamed) {
-						if (stream.next() !== '=') {
-							state.hasError = true;
-							return "invalidchar";
-						}
-						state.attributeNamed = false;
-						state.attributeAssigned = true;
-						return "operator";
-					} else if (state.attributeAssigned) {
-						state.attributeAssigned = false;
-						stream.match(/\s+/, true);
-						if (!state.attributeValue) {
-							if (stream.peek() === '"') {
-								stream.eat('"');
-								state.attributeValue = true;
-								state.attributeDoubleQuote = true;
-								return "quote";
-							} else if (stream.peek() === "'") {
-								stream.eat("'");
-								state.attributeValue = true;
-								state.attributeDoubleQuote = false;
-								return "quote";
-							} else {
-								stream.match(/\S+/, true);
-								return "quote";
-							}
-						} else if (state.attributeCloseQuote && stream.peek() === (state.attributeDoubleQuote ? '"' : "'")) {
-							state.attributeCloseQuote = false;
-							state.attributeDoubleQuote = false;
-							return "quote";
-						} else {
-							state.match(state.attributeDoubleQuote ? /[^"]+/ : /[^']+/, true);
-							state.attributeValue = false;
-							state.attributeCloseQuote = true;
-							return "quote";
-						}
-					} else {
-						if (stream.match(/[a-zA-Z0-9\-\_\$]+\s+/, true)) {
-							if (stream.peek() === '=') {
-								state.attributeNamed = true;
-								return "variable";
-							} else {
-								return "quote";
-							}
-						} else if (stream.peek() === '"') {
-							if (!state.argumentValue) {
-								if (stream.peek() === '"') {
-									stream.eat('"');
-									state.argumentValue = true;
-									state.argumentDoubleQuote = true;
-									return "quote";
-								} else if (stream.peek() === "'") {
-									stream.eat("'");
-									state.argumentValue = true;
-									state.argumentDoubleQuote = false;
-									return "quote";
-								} else {
-									stream.match(/\S+/, true);
-									return "quote";
-								}
-							} else if (state.argumentCloseQuote && stream.peek() === (state.argumentDoubleQuote ? '"' : "'")) {
-								state.argumentCloseQuote = false;
-								state.argumentDoubleQuote = false;
-								return "quote";
-							} else {
-								state.match(state.argumentDoubleQuote ? /[^"]+/ : /[^']+/, true);
-								state.argumentValue = false;
-								state.argumentCloseQuote = true;
-								return "quote";
-							}
-
-						} else {
-							if (stream.match(/\}\}/, true)) {
-								state.inMoustache = false;
-								return "bracket";
-							}
-						}
-					}
-
-					while (stream.next() !== null) {
-						if (stream.match("{{", false)) {
-							break;
-						}
-					}
-					return null;
+					console.log("Bad end char");
+					stream.next();
+					return "invalidchar";
 				}
+				if (stream.match('}}', true)) {
+					state.argumentList = false;
+					state.inHandlebar = false;
+					return "bracket";
+				}
+				if (stream.match(/^[\w\d\-\_\$]+\s*=/, false)) {
+					stream.match(/^[\w\d\-\_\$]+/, true);
+					stream.eatSpace();
+					state.attributeAssignment = true;
+					state.argumentList = false;
+					return "variable";
+				}
+				if (state.attributeAssignment) {
+					state.attributeAssignment = false;
+					state.attributeValue = true;
+					if (stream.next() !== '=') {
+						console.log("Expected =");
+						return "invalidchar";
+					}
+					return "operator";
+				}
+				if (state.attributeValue) {
+					state.attributeValue = false;
+					if (stream.match(/^"([^\\"]|\\\\|\\")*"/, false)) {
+						stream.match(/^"([^\\"]|\\\\|\\")*"/, true);
+						stream.eatSpace();
+						return "quote";
+					}
+					if (stream.match(/^'([^\\']|\\\\|\\')*'/, true)) {
+						stream.eatSpace();
+						return "quote";
+					}
+					stream.match(/^[^\s]+/, true);
+					console.log("Invalid attribute value");
+					return "invalidchar";
+				}
+				if (state.argumentList) {
+					if (stream.match(/^"([^\\"]|\\\\|\\")*"/, false)) {
+						stream.match(/^"([^\\"]|\\\\|\\")*"/, true);
+						stream.eatSpace();
+						return "quote";
+					}
+					if (stream.match(/^'([^\\']|\\\\|\\')*'/, true)) {
+						stream.eatSpace();
+						return "quote";
+					}
+					if (stream.match(/^[A-Za-z0-9\._$]+/, true)) {
+						stream.eatSpace();
+						return "variable";
+					}
+				}
+				;
+				console.log("Bad data: ", stream.next(), state);
+				return "invalidchar";
 			}
 		};
-		return CodeMirror.overlayMode(CodeMirror.getMode(config, parserConfig.backdrop || "text/html"), mustacheOverlay);
+		return CodeMirror.overlayMode(mustacheOverlay, CodeMirror.getMode(config, parserConfig.backdrop || "text/html"));
+		//return mustacheOverlay;
 	});
 
 	LanguageManager.defineLanguage("handlebars", {
